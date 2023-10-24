28294fb466ef0893094bdfce1c71971926cbabc1
Алексей Булыгин <a.bulygin@headsandhands.ru>
2023-10-24 14:03:50 +0300

    добавил основу для функции checkAuthorizationUser

--- src/test/java/general_cases_for_tests/AuthorizationScenarios.kt
+++ src/test/java/general_cases_for_tests/AuthorizationScenarios.kt
@@ -1,8 +1,11 @@
 package general_cases_for_tests
 
+import LocatorType
+import TestFunctions.checkAvailableElement
+
 object AuthorizationScenarios {
 
-    fun checkAuthorizationUser(needAuthorizationUser: Boolean){
+    fun checkAuthorizationUser(needAuthorizationUser: Boolean) {
         /* тут должна быть логика проверки авторизации пользователя
         на входе одна и нужных булевых переменных, вторую нужно получить путем
         проверки доступности элемента
@@ -10,13 +13,19 @@ object AuthorizationScenarios {
          */
 
         // нужно описать 4 ветки
-        val testBoolean = false // переменная для примера, вам нужно будет получить ее через функцию и назвать иначе
-
+        var userIsAuthorization: Boolean
+        try {
+            // нам нужно получить true, что это сделать, нужно найти элемент, если он не будет
+            // найден, то должна обработаться ошибка и установить значение false
+            userIsAuthorization = checkAvailableElement("locator", LocatorType.XPATH)
+        } catch (e: org.openqa.selenium.NoSuchElementException) {
+            userIsAuthorization = false
+        }
         when {
-            needAuthorizationUser && testBoolean -> {}
-            needAuthorizationUser && !testBoolean -> {}
-            !needAuthorizationUser && testBoolean -> {}
-            !needAuthorizationUser && !testBoolean -> {}
+            needAuthorizationUser && userIsAuthorization -> {}
+            needAuthorizationUser && !userIsAuthorization -> {}
+            !needAuthorizationUser && userIsAuthorization -> {}
+            !needAuthorizationUser && !userIsAuthorization -> {}
         }
 
     }
